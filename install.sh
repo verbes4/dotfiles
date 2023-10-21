589f30d99a56b649f430983649325d51704bb2ae
verbes4 <no-bruh@protonmail.com>
2023-10-22 00:49:49 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 CONFDIR=~/.config
 
+echo "Elevate priviilges"
+sudo sleep 1
+
 mv .zshrc ~/.zshrc
 mv .Xresources ~/.Xresources
 cd Pictures
+mkdir ~/Pictures/
 mv 1689327828382530.png ~/Pictures/1689327828382530.png
 cd ..
+mkdir ~/.local/share/
 mv .local/share/rofi ~/.local/share/rofi
 
 cd .config
 mv Vencord $CONFDIR/Vencord
 mv btop $CONFDIR/btop
-rm $CONFDIR/i3
+rm -rf $CONFDIR/i3
 mv i3 $CONFNDIR/i3
-rm $CONFDIR/i3blocks
 mv i3blocks $CONFDIR/i3
 mv micro $CONFDIR/micro
 mv mpv $CONFDIR/mpv
@@ -22,4 +26,4 @@ mv picom $CONFDIR/picom
 mv rofi $CONFDIR/rofi
 mv spotify $CONFDIR/spotify
 
-sudo pacman -S --noconfirm rxvt firefox rofi zsh btop micro mpv neofetch picom pacman-contrib
+sudo pacman -S --noconfirm rxvt firefox rofi zsh btop micro mpv neofetch picom pacman-contrib feh
